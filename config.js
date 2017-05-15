395191d8e1e027bf5b6d4f0189c245673f52836e
SarahELAMRI <selamri26@gmail.com>
2017-05-15 14:22:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEzNjQ2MzM2MDQ0MTcxMjgx.C_srLQ.Wk8zxGFWbBkKuxXYXU_5_uF-Lj0',
   channel: 'channelid'
 }
