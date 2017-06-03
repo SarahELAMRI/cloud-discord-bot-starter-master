4daec3286624149700d624ab4d7d841d3b2afdba
SarahELAMRI <selamri26@gmail.com>
2017-06-03 17:26:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,19 +2,114 @@ const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
 
+var SpotifyWebApi = require('spotify-web-api-node')
+// trouver les clés id et secret
+var clientid = ''
+var clientSecret = ''
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientid,
+  clientSecret: clientSecret
+})
+var track
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
 
 client.on('message', msg => {
-  // Check if the message has been posted in a channel where the bot operates
-  // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-
-  // If message is hello, post hello too
-  if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hello to you too, fellow !')
+  if (msg.content.lastIndexOf('!spotify') !== -1) {
+    spotifyApi.clientCredentialsGrant()
+      .then(function (data) {
+        console.log('The access token expires in ' + data.body['expires_in'])
+        console.log('The access token is ' + data.body['access_token'])
+        spotifyApi.setAccessToken(data.body['access_token'])
+        if (msg.content.match('!spotify help*')) {
+          msg.channel.sendMessage('Taper !spotify track suivi de la recherche pour afficher le top 3')
+          msg.channel.sendMessage('Taper !spotify artiste suivi de la recherche pour afficher le top 3')
+          msg.channel.sendMessage('Taper !spotify album suivi de la recherche pour afficher le top 3')
+          //Pour tous 
+          msg.channel.sendMessage('Taper !spotify suivi de la recherche pour afficher le top 3')
+        
+  // Instruction Chanson
+        } else if (msg.content.match('!spotify track *') & !msg.content.match('Taper !spotify track suivi de la recherche pour afficher le top 3*')) {
+          track = msg.content.substring(msg.content.lastIndexOf('!spotify track ') + '!spotify track '.length, msg.content.length)
+          spotifyApi.searchTracks(track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des chansons : ' + track)
+              for (var i = 0; i < 3; i++) {
+                msg.channel.sendMessage('"' + data.body.tracks.items[i].name + '" de ' + data.body.tracks.items[i].artists[0].name)
+              }
+            }, function (err) {
+              console.error(err)
+            })
+  //Instruction Artiste
+        } else if (msg.content.match('!spotify artiste *') & !msg.content.match('Taper !spotify artiste suivi de la recherche pour afficher le top 3*')) {
+          track = msg.content.substring(msg.content.lastIndexOf('!spotify artiste ') + '!spotify artiste '.length, msg.content.length)
+          spotifyApi.searchArtists(track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des artistes : ' + track)
+              for (var i = 0; i < 3; i++) {
+                if (data.body.artists.items[i].genres[0] !== undefined) {
+                  msg.channel.sendMessage('"' + data.body.artists.items[i].name + '" Genre : ' + data.body.artists.items[i].genres[0])
+                } else {
+                  msg.channel.sendMessage(data.body.artists.items[i].name)
+                }
+              }
+            }, function (err) {
+              console.error(err)
+            })
+  //Instructions Album
+        } else if (msg.content.match('!spotify album *') & !msg.content.match('Taper !spotify album suivi de la recherche pour afficher le top 3*')) {
+          track = msg.content.substring(msg.content.lastIndexOf('!spotify album ') + '!spotify album '.length, msg.content.length)
+          spotifyApi.searchTracks('album:' + track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des albums : ' + track)
+              for (var i = 0; i < 3; i++) {
+                msg.channel.sendMessage('"' + data.body.tracks.items[0].album.name + '" de ' + data.body.tracks.items[i].album.artists[0].name)
+              }
+            }, function (err) {
+              console.error(err)
+            })
+          
+ //Instruction pour la recherche avec chanson, artiste et album 
+        } else if (msg.content.match('!spotify *') & !msg.content.match('Taper !spotify suivi de la recherche pour afficher le top 3*')) {
+          track = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
+ // Recherche Albums
+          spotifyApi.searchTracks('album:' + track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des albums : ' + track)
+              for (var i = 0; i < 3; i++) {
+                msg.channel.sendMessage('"' + data.body.tracks.items[i].album.name + '" de ' + data.body.tracks.items[i].artists[0].name)
+              }
+            }, function (err) {
+              console.error(err)
+            })
+ // Recherche Chansons
+          spotifyApi.searchTracks('track:' + track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des chansons : ' + track)
+              for (var i = 0; i < 3; i++) {
+                msg.channel.sendMessage('"' + data.body.tracks.items[i].name + '" de ' + data.body.tracks.items[i].artists[0].name)
+              }
+            }, function (err) {
+              console.error(err)
+            })
+ //Recherche Artistes
+          spotifyApi.searchArtists(track)
+            .then(function (data) {
+              msg.channel.sendMessage('Top 3 des artistes : ' + track)
+              for (var i = 0; i < 3; i++) {
+                msg.channel.sendMessage('"' + data.body.artists.items[i].name + '"')
+              }
+            }, function (err) {
+              console.error(err)
+            })
+        }
+      // fin partie avec les trois paramètres
+      }, function (err) {
+        console.log('Something went wrong when retrieving an access token', err.message)
+      })
   }
-})
-
+}
+)
 client.login(config.token)
