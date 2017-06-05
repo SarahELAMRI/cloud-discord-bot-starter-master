221bac33bd3e4f0bf6d671e71b0b1eb253e65536
SarahELAMRI <selamri26@gmail.com>
2017-06-05 19:52:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,14 +20,14 @@ client.on('message', msg => {
         console.log('The access token is ' + data.body['access_token'])
         spotifyApi.setAccessToken(data.body['access_token'])
         if (msg.content.match('!spotify help*')) {
-          msg.channel.sendMessage('Taper !spotify track suivi de la recherche pour afficher le top 3')
-          msg.channel.sendMessage('Taper !spotify artiste suivi de la recherche pour afficher le top 3')
-          msg.channel.sendMessage('Taper !spotify album suivi de la recherche pour afficher le top 3')
+          msg.channel.sendMessage('Taper !spotify track suivi de la recherche')
+          msg.channel.sendMessage('Taper !spotify artiste suivi de la recherche')
+          msg.channel.sendMessage('Taper !spotify album suivi de la recherche')
           //Pour tous 
-          msg.channel.sendMessage('Taper !spotify suivi de la recherche pour afficher le top 3')
+          msg.channel.sendMessage('Taper !spotify suivi de la recherche')
         
   // Instruction Chanson
-        } else if (msg.content.match('!spotify track *') & !msg.content.match('Taper !spotify track suivi de la recherche pour afficher le top 3*')) {
+        } else if (msg.content.match('!spotify track *') & !msg.content.match('Taper !spotify track suivi de la recherche*')) {
           track = msg.content.substring(msg.content.lastIndexOf('!spotify track ') + '!spotify track '.length, msg.content.length)
           spotifyApi.searchTracks(track)
             .then(function (data) {
@@ -39,7 +39,7 @@ client.on('message', msg => {
               console.error(err)
             })
   //Instruction Artiste
-        } else if (msg.content.match('!spotify artiste *') & !msg.content.match('Taper !spotify artiste suivi de la recherche pour afficher le top 3*')) {
+        } else if (msg.content.match('!spotify artiste *') & !msg.content.match('Taper !spotify artiste suivi de la recherche*')) {
           track = msg.content.substring(msg.content.lastIndexOf('!spotify artiste ') + '!spotify artiste '.length, msg.content.length)
           spotifyApi.searchArtists(track)
             .then(function (data) {
@@ -55,7 +55,7 @@ client.on('message', msg => {
               console.error(err)
             })
   //Instructions Album
-        } else if (msg.content.match('!spotify album *') & !msg.content.match('Taper !spotify album suivi de la recherche pour afficher le top 3*')) {
+        } else if (msg.content.match('!spotify album *') & !msg.content.match('Taper !spotify album suivi de la recherche*')) {
           track = msg.content.substring(msg.content.lastIndexOf('!spotify album ') + '!spotify album '.length, msg.content.length)
           spotifyApi.searchTracks('album:' + track)
             .then(function (data) {
@@ -68,7 +68,7 @@ client.on('message', msg => {
             })
           
  //Instruction pour la recherche avec chanson, artiste et album 
-        } else if (msg.content.match('!spotify *') & !msg.content.match('Taper !spotify suivi de la recherche pour afficher le top 3*')) {
+        } else if (msg.content.match('!spotify *') & !msg.content.match('Taper !spotify suivi de la recherche*')) {
           track = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
  // Recherche Albums
           spotifyApi.searchTracks('album:' + track)
