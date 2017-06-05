7e33f5ac0823b7819a839749329ab230f0dcda27
SarahELAMRI <selamri26@gmail.com>
2017-06-05 20:21:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,34 +8,33 @@ var translate = require('@google-cloud/translate')({
   key: API_KEY
 })
 
-// https://www.npmjs.com/package/@google-cloud/translate
 
-var elem
+var tx
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content.match('!trad *')) {
+  if (msg.content.match('!traduire *')) {
     console.log(msg.content)
-    if (msg.content.match('!trad help*')) {
-      msg.channel.sendMessage('Taper !trad Votre_texte traduction du texte en anglais')
-    } else if (msg.content.match('!trad lg:[A-Za-z][A-Za-z] *') && !msg.content.match('Taper !trad Votre_texte traduction du texte en anglais')) {
-      elem = msg.content.substring(11, msg.content.length)
+    if (msg.content.match('!traduire help*')) {
+      msg.channel.sendMessage('Taper !traduire Votre_texte traduction du texte en anglais')
+    } else if (msg.content.match('!traduire lg:[A-Za-z][A-Za-z] *') && !msg.content.match('Taper !traduire Votre_texte traduction du texte en anglais')) {
+      tx = msg.content.substring(11, msg.content.length)
       console.log(msg.content)
       console.log('ici')
       var language = msg.content.substring(9, 11, msg.content.length)
-      translate.translate(elem, language, function (err, translation) {
+      translate.translate(tx, language, function (err, translation) {
         if (!err) {
           msg.channel.sendMessage(translation)
         }
       })
-    } else if (msg.content.match('!trad*') && !msg.content.match('Taper !trad Votre_texte traduction du texte en anglais')) {
+    } else if (msg.content.match('!traduire*') && !msg.content.match('Taper !traduire Votre_texte traduction du texte en anglais')) {
       console.log(msg.content)
       console.log('la')
-      elem = msg.content.substring(msg.content.lastIndexOf('!trad ') + '!trad '.length, msg.content.length)
-      translate.translate(elem, 'en', function (err, translation) {
+      tx = msg.content.substring(msg.content.lastIndexOf('!traduire ') + '!traduire '.length, msg.content.length)
+      translate.translate(tx, 'en', function (err, translation) {
         if (!err) {
           msg.channel.sendMessage(translation)
         }
