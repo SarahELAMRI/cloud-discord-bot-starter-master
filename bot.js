a02aa9712aab7abc0febb0a0bea39821c4b97284
Babaloa <alvine.venkatramiah@isep.fr>
2017-06-06 19:42:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client()
 // clé pour API
 var API_KEY = 'AIzaSyCvwWHyl3-w3fVPCgOrWbfqWFTi7fxJ_yg'
 
-var translate = require('@google-cloud/translate')({
+var translate = require('google-translator')({
   key: API_KEY
 })
 
