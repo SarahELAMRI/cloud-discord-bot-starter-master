8599971fc0496b7ec215888f066e370dc6551227
Babaloa <alvine.venkatramiah@isep.fr>
2017-06-10 23:33:18 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', msg => {
                      }
                        else {
 			       for (var i = 0; i < 3; i++) {
-				       msg.channel.sendMessage('"' + result[i])
+				       msg.channel.sendMessage('"' + result[i].id)
               			}
 			}
 	    })
