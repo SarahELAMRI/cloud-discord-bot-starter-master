1be19808593e5798dced87552fa0ecf56309830a
Babaloa <alvine.venkatramiah@isep.fr>
2017-06-10 23:01:02 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,37 @@ const client = new Discord.Client()
 //$ npm install youtube-node
 
 
-var YouTube = require('youtube-node');
+var YouTube = require('youtube-node')
 
-var youTube = new YouTube();
+var youTube = new YouTube()
 
-youTube.setKey('AIzaSyBWTMi3IPxFmIe1yaVg7lcHEmztQm5m0oc'); // clé API Youtube
+youTube.setKey('AIzaSyBVQcdqQnbBH8jtfLAqOWfcqf3Lz5CDqlA') // clé API Youtube
 
 var track
 
 // Instructions bot 
+client.on('ready', () => {
+	    console.log(`Logged in as ${client.user.username}!`)
+	})
+
+
 client.on('message', msg => {
-    if (msg.content.match('!youtube *') & !msg.content.match('Taper !youtube suivi de la recherche*')) {
+    if (msg.content.lastIndexOf('!youtube') !== -1) {
           track = msg.content.substring(msg.content.lastIndexOf('!youtube ') + '!youtube '.length, msg.content.length)
+            youTube.search(track,3, function(error, result)) {
+                     if (error) {
+                           console.log(error);
+                     }
+                       else {
+                           console.log(JSON.stringify(result, null, 2))
+              }
+        ])
+    }
+      
+})
+
+client.login(config.token)
+
       
 //recherche des 3 resultats
 //youTube.search( track , 3, function(error, result) {
@@ -30,16 +49,3 @@ client.on('message', msg => {
 //});
         
 //autre essai
- youTube.search(track)
-            .then(function (data) {
-              msg.channel.sendMessage('Top 3 : ' + track)
-              for (var i = 0; i < 3; i++) {
-                msg.channel.sendMessage('"' + data.body.tracks.items[i].album.name + '" de ' + data.body.tracks.items[i].artists[0].name)
-              }
-            }, function (err) {
-              console.error(err)
-            })
-      
-}
-
-client.login(config.token)
