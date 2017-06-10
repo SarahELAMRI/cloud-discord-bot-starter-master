28be9832450c3ef17dd7fe492400b9ffa0431a7f
SarahELAMRI <selamri26@gmail.com>
2017-06-10 20:30:14 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -2,38 +2,33 @@ const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`)
-});
+//$ npm install youtube-node
 
-client.on('message', msg => {
-  // Check if the message has been posted in a channel where the bot operates
-  // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-  
-    function defineRequest() {
-    // See full sample for buildApiRequest() code, which is not 
-// specific to a particular youtube or youtube method.
 
 var YouTube = require('youtube-node');
 
 var youTube = new YouTube();
 
-youTube.setKey('AIzaSyBWTMi3IPxFmIe1yaVg7lcHEmztQm5m0oc'); // clé obtenu avec google 
-  // ajouter avriable de recherche  
-// If message is nom de la recherche, post results
-  if (msg.content === 'hello') { // remplacer hello par la variable de recherche
-    
-youTube.search('World War z Trailer', 3, function(error, result) { // changer avec la recherche
+youTube.setKey('AIzaSyBWTMi3IPxFmIe1yaVg7lcHEmztQm5m0oc'); // clé API Youtube
+
+var track
+
+// Instructions bot 
+client.on('message', msg => {
+    if (msg.content.match('!youtube *') & !msg.content.match('Taper !youtube suivi de la recherche*')) {
+          track = msg.content.substring(msg.content.lastIndexOf('!youtube ') + '!youtube '.length, msg.content.length)
+      
+//recherche des 3 resultats
+youTube.search( track , 3, function(error, result) {
   if (error) {
     console.log(error);
   }
   else {
     console.log(JSON.stringify(result, null, 3));
   }
+  
 });
-    msg.channel.sendMessage('Here is the result of your research, fellow !')
-  } 
-})
+      
+}
 
 client.login(config.token)
