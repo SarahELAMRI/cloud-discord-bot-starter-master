0fb8c3b26698e40a22f4aef957860ca9c71da62c
Babaloa <alvine.venkatramiah@isep.fr>
2017-06-10 19:18:24 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const config = require('./config.js')
 const client = new Discord.Client()
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : 'fcecfc72172e4cd267473117a17cbd4d',
-  clientSecret : 'a6338157c9bb5ac9c71924cb2940e1a7',
+  clientId : '685eba1d89104afda1af8026b9d43f90',
+  clientSecret : '4015d54a8f004ba2b5048de3b7ab6310',
 })
 
 var track
@@ -20,55 +20,10 @@ client.on('message', msg => {
         console.log('The access token is ' + data.body['access_token'])
         spotifyApi.setAccessToken(data.body['access_token'])
         if (msg.content.match('!spotify help*')) {
-          msg.channel.sendMessage('Taper !spotify track suivi de la recherche')
-          msg.channel.sendMessage('Taper !spotify artiste suivi de la recherche')
-          msg.channel.sendMessage('Taper !spotify album suivi de la recherche')
-          //Pour tous 
           msg.channel.sendMessage('Taper !spotify suivi de la recherche')
         
-  // Instruction Chanson
-        } else if (msg.content.match('!spotify track *') & !msg.content.match('Taper !spotify track suivi de la recherche*')) {
-          track = msg.content.substring(msg.content.lastIndexOf('!spotify track ') + '!spotify track '.length, msg.content.length)
-          spotifyApi.searchTracks(track)
-            .then(function (data) {
-              msg.channel.sendMessage('Top 3 des chansons : ' + track)
-              for (var i = 0; i < 3; i++) {
-                msg.channel.sendMessage('"' + data.body.tracks.items[i].name + '" de ' + data.body.tracks.items[i].artists[0].name)
-              }
-            }, function (err) {
-              console.error(err)
-            })
-  //Instruction Artiste
-        } else if (msg.content.match('!spotify artiste *') & !msg.content.match('Taper !spotify artiste suivi de la recherche*')) {
-          track = msg.content.substring(msg.content.lastIndexOf('!spotify artiste ') + '!spotify artiste '.length, msg.content.length)
-          spotifyApi.searchArtists(track)
-            .then(function (data) {
-              msg.channel.sendMessage('Top 3 des artistes : ' + track)
-              for (var i = 0; i < 3; i++) {
-                if (data.body.artists.items[i].genres[0] !== undefined) {
-                  msg.channel.sendMessage('"' + data.body.artists.items[i].name + '" Genre : ' + data.body.artists.items[i].genres[0])
-                } else {
-                  msg.channel.sendMessage(data.body.artists.items[i].name)
-                }
-              }
-            }, function (err) {
-              console.error(err)
-            })
-  //Instructions Album
-        } else if (msg.content.match('!spotify album *') & !msg.content.match('Taper !spotify album suivi de la recherche*')) {
-          track = msg.content.substring(msg.content.lastIndexOf('!spotify album ') + '!spotify album '.length, msg.content.length)
-          spotifyApi.searchTracks('album:' + track)
-            .then(function (data) {
-              msg.channel.sendMessage('Top 3 des albums : ' + track)
-              for (var i = 0; i < 3; i++) {
-                msg.channel.sendMessage('"' + data.body.tracks.items[0].album.name + '" de ' + data.body.tracks.items[i].album.artists[0].name)
-              }
-            }, function (err) {
-              console.error(err)
-            })
-          
- //Instruction pour la recherche avec chanson, artiste et album 
-        } else if (msg.content.match('!spotify *') & !msg.content.match('Taper !spotify suivi de la recherche*')) {
+        }
+      if (msg.content.match('!spotify *') & !msg.content.match('Taper !spotify suivi de la recherche*')) {
           track = msg.content.substring(msg.content.lastIndexOf('!spotify ') + '!spotify '.length, msg.content.length)
  // Recherche Albums
           spotifyApi.searchTracks('album:' + track)
