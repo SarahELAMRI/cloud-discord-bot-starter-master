588b61aa7053b41d7c86e40feebade2dba0b4dfd
SarahELAMRI <selamri26@gmail.com>
2017-06-10 19:42:32 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
       .then(function (data) {
         console.log('The access token expires in ' + data.body['expires_in'])
         console.log('The access token is ' + data.body['access_token'])
-        spotifyApi.setAccessToken(data.body['access_token'])
+        youtubeApi.setAccessToken(data.body['access_token'])
         if (msg.content.match('!youtube help*')) {
           msg.channel.sendMessage('Taper !youtube track suivi de la recherche')
           msg.channel.sendMessage('Taper !youtube artiste suivi de la recherche')
